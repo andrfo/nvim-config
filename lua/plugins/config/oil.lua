c4b46a6c412e72998f704582aa36bbfe559398d4
André Fosvold <andre.fosvold@mattilsynet.no>
2025-03-06 09:37:40 +0100

    show hidden files and folders in oil

--- lua/plugins/config/oil.lua
+++ lua/plugins/config/oil.lua
@@ -1,6 +1,10 @@
 -- oil.nvim configuration
 return {
   default_file_explorer = true,
+  -- Show hidden files by setting the following option
+  view_options = {
+    -- Show files and directories that start with "."
+    show_hidden = true,
+  },
   -- Add any additional oil.nvim options here.
 }
-
