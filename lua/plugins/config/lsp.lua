89f670d78cfe82e95d8599450fbd0614fa5711db
André Fosvold <andre.fosvold@mattilsynet.no>
2025-08-07 08:35:44 +0200

    fix: double lsp

--- lua/plugins/config/lsp.lua
+++ lua/plugins/config/lsp.lua
@@ -33,46 +33,40 @@ require("mason-lspconfig").setup({
   ensure_installed = {
     "lua_ls",
     "pyright",
-    "kotlin_language_server", 
+    "kotlin_language_server",
     "gopls",
     "terraformls",
     "marksman",
     -- DON'T include rust_analyzer here - rustaceanvim handles it
   },
+  -- CRITICAL: Use handlers to prevent automatic setup of rust_analyzer
+  handlers = {
+    -- Default handler for most servers
+    function(server_name)
+      -- Skip rust_analyzer completely - rustaceanvim handles it
+      if server_name == "rust_analyzer" then
+        return -- This prevents the server from being set up
+      end
+      -- Set up all other servers with default config
+      require("lspconfig")[server_name].setup({
+        on_attach = on_attach,
+      })
+    end,
+    -- Specific handlers for servers that need custom config
+    ["lua_ls"] = function()
+      require("lspconfig").lua_ls.setup({
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' },
+            },
+          },
+        },
+      })
+    end,
+  }
 })
 
--- Set up language servers manually (excluding rust_analyzer)
-local lspconfig = require("lspconfig")
-
-lspconfig.lua_ls.setup({
-  on_attach = on_attach,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' },
-      },
-    },
-  },
-})
-
-lspconfig.pyright.setup({
-  on_attach = on_attach,
-})
-
-lspconfig.terraformls.setup({
-  on_attach = on_attach,
-})
-
-lspconfig.kotlin_language_server.setup({
-  on_attach = on_attach,
-})
-
-lspconfig.gopls.setup({
-  on_attach = on_attach,
-})
-
-lspconfig.marksman.setup({
-  on_attach = on_attach,
-})
-
+-- DON'T manually set up any servers here - handlers take care of it
 -- DON'T set up rust_analyzer here - rustaceanvim handles it completely
